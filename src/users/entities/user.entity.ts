52cf65ababc7cf6d5f1420ab7a877f21519731d4
yhsooi <didgmltn7769@naver.com>
2022-01-09 21:02:08 +0900

    [Edit] user logic & entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,61 @@
-export class User {}
+import { Board } from '@src/boards/entities/board.entity';
+import { Comment } from '@src/comments/entities/comment.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    unique: true,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+    unique: true,
+  })
+  nickname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    unique: true,
+  })
+  email: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Board, (board) => board.user)
+  boards: Board[];
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+}
