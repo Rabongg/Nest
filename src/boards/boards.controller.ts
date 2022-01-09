dd125a6e4aad062681f033eb62410e42d38e4c7c
yhsooi <didgmltn7769@naver.com>
2022-01-09 21:01:45 +0900

    [Edit] board logic & entity

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,1 +1,40 @@
-export class Board {}
+import { User } from '@src/users/entities/user.entity';
+import { Comment } from '@src/comments/entities/comment.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+  })
+  title: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.boards)
+  @JoinColumn({ name: 'user_id' })
+  user: number;
+
+  @OneToMany(() => Comment, (comment) => comment.board)
+  comments: Comment[];
+}
