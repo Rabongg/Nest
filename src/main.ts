e5dc148e6fe7070a88c0337977da307d4e0176f3
yhsooi <didgmltn7769@naver.com>
2022-01-10 22:15:45 +0900

    [ADD] global pipes & session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(
+    session({
+      name: 'Fanta',
+      secret: 'An0+n4lh2Ag1tEAs9ad1',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 3,
+        httpOnly: true,
+        sameSite: 'strict',
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
