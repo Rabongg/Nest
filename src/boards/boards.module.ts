ec945cdba90285c8543d69cdc11a44f51203e242
yhsooi <didgmltn7769@naver.com>
2022-01-11 23:47:02 +0900

    [ADD] imports board category table

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,10 @@ import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
+import { BoardCategory } from './entities/board-category';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board, BoardCategory])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
