07bf4e14a567e30c581ceba5d0dcef61c51155de
yhsooi <didgmltn7769@naver.com>
2022-01-14 16:15:01 +0900

    [EDIT] socket emit

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,27 +1,31 @@
-// import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
-// @WebSocketGateway()
-// export class ChatGateway {
-//   @SubscribeMessage('message')
-//   handleMessage(client: any, payload: any): string {
-//     return 'Welcome!';
-//   }
-// }
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'net';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(8100, { namespace: 'chat', cors: true })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer() server: Server;
 
-  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
-  public users: number = 0;
+  public users = {};
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('socket connected');
+  }
+
+  handleDisconnect(client: Socket) {
+    const room = client.data.roomNumber;
+    const userData = `${client.data.nickname}님이 퇴장했습니다.`;
+    console.log('socket disconnected');
+    this.users[`${room}`]--;
+    this.server.to(room).emit('exit', userData, this.users[`${room}`]);
+  }
 
   @SubscribeMessage('entrance')
   connectSomeone(
@@ -31,12 +35,16 @@ export class ChatGateway {
     const [nickname, room]: string[] = data;
     console.log(`${nickname}님이 코드: ${room}방에 접속했습니다.`);
     const userData = `${nickname}님이 입장했습니다.`;
-    this.users++;
-    this.server.emit(`entrance(${room})`, userData, this.users);
+    client.join(room);
+    client.data.nickname = nickname;
+    client.data.roomNumber = room;
+    if (this.users[`${room}`]) this.users[`${room}`]++;
+    else this.users[`${room}`] = 1;
+    this.server.to(room).emit('enter', userData, this.users[`${room}`]);
   }
 
   @SubscribeMessage('send')
-  sendMessage(@MessageBody() data: string, @ConnectedSocket() client) {
+  sendMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
     const [room, nickname, message] = data;
     console.log(`${client.id} : ${data}`);
     client.broadcast.emit(room, [nickname, message]);
