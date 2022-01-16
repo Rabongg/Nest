6a7b025c19cd1d931aa179a90d981a91d1c0213f
yhsooi <didgmltn7769@naver.com>
2022-01-16 23:47:04 +0900

    [EDIT] test code

--- src/boards/boards.controller.spec.ts
+++ src/boards/boards.controller.spec.ts
@@ -1,20 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { Board } from './entities/board.entity';
+
+const MockBoardRepository = () => ({
+  createQueryBuilder: jest.fn().mockReturnValue({
+    innerJoin: jest.fn().mockReturnThis(),
+    select: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    getMany: jest.fn().mockReturnThis(),
+    andWhere: jest.fn().mockReturnThis(),
+    getOne: jest.fn().mockReturnThis(),
+    execute: jest.fn().mockReturnThis(),
+    offset: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockReturnThis(),
+    update: jest.fn().mockReturnThis(),
+    set: jest.fn().mockReturnThis(),
+  }),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('BoardsController', () => {
-  let controller: BoardsController;
+  let controller1: BoardsController;
+  let controller2: BoardsController;
+  let boardRepository: MockRepository<Board>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BoardsController],
-      providers: [BoardsService],
+      providers: [
+        BoardsService,
+        {
+          provide: getRepositoryToken(Board),
+          useValue: MockBoardRepository(),
+        },
+      ],
     }).compile();
 
-    controller = module.get<BoardsController>(BoardsController);
+    controller1 = module.get<BoardsController>(BoardsController);
+    controller2 = module.get<BoardsController>(BoardsController);
+    boardRepository = module.get<MockRepository<Board>>(
+      getRepositoryToken(Board),
+    );
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be same', () => {
+    expect(controller1).toEqual(controller2);
+  });
+
+  it('should create a new board', async () => {
+    expect(controller1).toEqual(controller2);
   });
 });
--- src/boards/boards.service.spec.ts
+++ src/boards/boards.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { BoardsService } from './boards.service';
+import { Board } from './entities/board.entity';
 
 describe('BoardsService', () => {
   let service: BoardsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardsService],
+      providers: [
+        BoardsService,
+        { provide: getRepositoryToken(Board), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<BoardsService>(BoardsService);
